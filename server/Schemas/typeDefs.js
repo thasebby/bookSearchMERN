fd52998fe2e01bda27eb848f4fa56c5bfac7f607
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-04-04 00:55:24 -0400

    finished the server directory

--- server/Schemas/typeDefs.js
+++ server/Schemas/typeDefs.js
@@ -9,17 +9,39 @@ const typeDefs = `
     }
 
     type Book {
-
+        _id: ID
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
     }
 
     type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        saveBooks: [Book]
+    }
 
+    input SavedBooks {
+        authors: [String]
+        description: String
+        title: String
+        bookId: String
+        image: String
+        link: String
     }
 
     type Mutation {
-        
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SavedBooks): User
+        removeBook(bookId): User
     }
-`
+`;
 
 // export typeDefs
 module.exports = typeDefs;
