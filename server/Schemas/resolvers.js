ca28b10d6bf8072f2945c9fb8fdf874431e74225
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-04-04 00:13:40 -0400

    started the server directory

---
+++ server/Schemas/resolvers.js
@@ -0,0 +1,57 @@
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw AuthenticationError;
+    },
+  },
+  Mutation: {
+    createUser: async (parent, args ) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const profile = await User.findOne({ email });
+
+      if (!profile) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await profile.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(profile);
+      return { token, profile };
+    },
+    saveBook: async (parent, { user, book }, context) => {
+      if (context.user){
+        return User.findOneAndUpdate(
+          { _id: context.user._id},
+          { $addToSet: { savedBooks: book}},
+          {runValidators: true, new: true},
+        );
+      }
+    },
+    deleteBook: async (parent, { bookId }, context) => {
+     if (context.user) {
+      return User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: bookId }} },
+        { new: true }
+      )
+     }
+      throw AuthenticationError;
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startApolloserver = async () => {
+  await server.start();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  // app.use(routes);
+
+  db.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+};
+
+startApolloserver();
